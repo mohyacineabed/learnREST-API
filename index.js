d2351d5aa9b58e8530af6a314343817c28e337d8
Yacine Abed <yacine.abed@student.univ-temouchent.edu.dz>
2025-03-03 11:12:26 +0100

    update index.js
    connect to mongodb using mongoose

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 // require express to use it
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // require router
 const routes = require('./routes/api');
 
 //set up express app
 const app = express();
+
+// connect to mongodb:
+mongoose.connect('mongodb://localhost/ninjago'); 
+                 // if ninjago does not exist, mongoose will create it
+// override mongoose promise
+mongoose.Promise = global.Promise;
+
 
 app.use(bodyParser.json());
 
